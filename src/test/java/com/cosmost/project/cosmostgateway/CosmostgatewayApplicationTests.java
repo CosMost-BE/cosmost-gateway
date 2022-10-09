0117445b32c4f0ffaa421ee9c6876b56f189001b
ChanYoungHong <cksdud9883@naver.com>
2022-10-10 01:20:05 +0900

    [create] first setting

---
+++ src/test/java/com/cosmost/project/cosmostgateway/CosmostgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosmost.project.cosmostgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmostgatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
