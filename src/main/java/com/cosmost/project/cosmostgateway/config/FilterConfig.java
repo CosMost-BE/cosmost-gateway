0fcd0c2d8df1e8288e832be727d5b52de7094b83
chanyoung Hong <cksdud9883@naver.com>
2022-10-19 22:37:26 +0900

    [create] add FilterConfig

---
+++ src/main/java/com/cosmost/project/cosmostgateway/config/FilterConfig.java
@@ -0,0 +1,17 @@
+package com.cosmost.project.cosmostgateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route()
+                .build();
+    }
+}
