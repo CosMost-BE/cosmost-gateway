fe3941740c1bd5cc142df491da0d295b653c5f52
chanyoung Hong <cksdud9883@naver.com>
2022-10-20 22:42:33 +0900

    [update] CustomFilter Setting

--- src/main/java/com/cosmost/project/cosmostgateway/filter/CustomFilter.java
+++ src/main/java/com/cosmost/project/cosmostgateway/filter/CustomFilter.java
@@ -1,4 +1,36 @@
 package com.cosmost.project.cosmostgateway.filter;
 
-public class CustomFilter {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.io.ObjectInputFilter;
+
+@Component
+@Slf4j
+public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Config> {
+
+    public static class Config {
+
+    }
+
+    public CustomFilter() {
+        super(Config.class);
+    }
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+
+            }));
+        });
+    }
 }
