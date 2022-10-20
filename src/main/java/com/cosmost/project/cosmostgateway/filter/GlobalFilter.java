3c632f96c5bfc6cf8e51da04d7ddb082c659e8ca
chanyoung Hong <cksdud9883@naver.com>
2022-10-20 22:41:14 +0900

    [update] GloblFilter setting

--- src/main/java/com/cosmost/project/cosmostgateway/filter/GlobalFilter.java
+++ src/main/java/com/cosmost/project/cosmostgateway/filter/GlobalFilter.java
@@ -1,4 +1,33 @@
 package com.cosmost.project.cosmostgateway.filter;
 
-public class GlobalFilter  {
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import reactor.core.publisher.Mono;
+
+public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
+
+    public GlobalFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(GlobalFilter.Config config) {
+        return (((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+
+            }));
+        }));
+    }
+
+    public static class Config {
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
+    }
+
 }
